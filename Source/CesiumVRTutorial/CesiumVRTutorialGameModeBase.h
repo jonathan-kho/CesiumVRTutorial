d98a8a0c6a90a392c1d91ecf869c7655034aa384
Jonathan Kho <jonathan.r.kho@gmail.com>
2021-11-29 18:29:27 -0500

    Commit the tutorial.

---
+++ Source/CesiumVRTutorial/CesiumVRTutorialGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CesiumVRTutorialGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CESIUMVRTUTORIAL_API ACesiumVRTutorialGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
